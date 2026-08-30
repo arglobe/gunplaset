0c543cce320b22b93bc6aa6224f757316a3e209a
arglobe <arglobe@gmail.com>
2026-08-30 09:44:45 +0900

    Add files via upload

---
+++ my_collection.js
@@ -0,0 +1,442 @@
+{
+  "525": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "564": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "592": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "604": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "629": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "631": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "636": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "639": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "642": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "646": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "647": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "855": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "856": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "949": {
+    "backlog": 2,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "953": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "1073": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "1100": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "1255": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "1618": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "1816": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2014": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "2454": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2525": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2705": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2728": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2809": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "2939": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "3004": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "3103": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "3268": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "3532": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "3994": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4028": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4029": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4196": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4200": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4306": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4314": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4361": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4368": {
+    "backlog": 2,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4435": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4480": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4504": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4516": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4526": {
+    "backlog": 2,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4556": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4558": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4625": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4653": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4919": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "4954": {
+    "backlog": 2,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  },
+  "4955": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "5089": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "5182": {
+    "backlog": 0,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": true,
+    "notes": "",
+    "customPrice": null
+  },
+  "5250": {
+    "backlog": 1,
+    "inProgress": 0,
+    "built": 0,
+    "wishlist": false,
+    "notes": "",
+    "customPrice": null
+  }
+}
